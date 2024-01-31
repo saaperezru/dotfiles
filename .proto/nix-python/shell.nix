ec0c03421e7b399602f2aa2c33776f4e5233903f
santi <saaperezru@gmail.com>
2024-01-31 13:07:21 -0500

    Add extra plotting libraries to Python template environment

--- .proto/nix-python/shell.nix
+++ .proto/nix-python/shell.nix
@@ -1,5 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
-    buildInputs = with pkgs.buildPackages; [ python310 python310Packages.matplotlib python310Packages.numpy python310Packages.pandas python310Packages.statsmodels python310Packages.scipy python310Packages.seaborn poetry ];
+    buildInputs = with pkgs.buildPackages; [ python310 python310Packages.matplotlib python310Packages.altair python310Packages.plotly python310Packages.numpy python310Packages.pandas python310Packages.statsmodels python310Packages.scipy python310Packages.seaborn poetry ];
 }
